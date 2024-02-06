5b412af50c36a024c291a07f10313e6466a3347b
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-06 17:26:06 -0500

    added appropriate error statuscodes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { port, env } = require('./config/vars');
 const app = require('./config/express');
 const { sequelize } = require('./models'); // listen to requests
 
-sequelize.sync({ force: false }).then( () => {
+sequelize.sync({ alter: true }).then( () => {
     app.listen(port, () => console.log(`Server started on port ${port} (${env})`));
   });
 
