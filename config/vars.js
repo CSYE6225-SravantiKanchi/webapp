f94c7da8a22a5bf4e80d419987948e875f6799d3
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-13 18:25:58 -0500

    fixed github workflow for integration tests

--- config/vars.js
+++ config/vars.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 const appPackage = require('../package.json');
 
-require('dotenv-safe').config({
+require('dotenv').config({
   path: path.join(__dirname, '../.env'),
-  sample: path.join(__dirname, '../.env.example'),
 });
 
 module.exports = {
