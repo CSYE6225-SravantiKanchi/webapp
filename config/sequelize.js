66b31b67f067ca0c1407a5c255a35a241f99a3da
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-14 08:49:08 -0500

    fixed github workflow for integration tests

--- config/sequelize.js
+++ config/sequelize.js
@@ -5,8 +5,6 @@ const { sqlUri, database } = require('./vars');
  *followed https://sequelize.org/docs/v6/getting-started/
  */
 
-console.log(`this is the sql uri ${process.env.SQL_URI}${process.env.DATABASE}`);
-
 const sequelize = new Sequelize(`${sqlUri}${database}`);
 
 exports.sequelize = sequelize;
