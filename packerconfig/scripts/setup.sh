df68e2838acf47724f4e00a4d9d892be71478554
sravanti2300 <sravanti.kanchi@gmail.com>
2024-02-19 19:05:23 -0500

    changes to add sql db password to secrets of git

--- packerconfig/scripts/setup.sh
+++ packerconfig/scripts/setup.sh
@@ -16,10 +16,7 @@ sudo systemctl enable mysqld.service
 mysqladmin -u root password "root" #TODO - check what to do here??
 
 
-#npm setup
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-# export NVM_DIR="$HOME/.nvm"
-#         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-#         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"    
+#node v18.X setup
+curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -   
 sudo dnf install -y nodejs
 node --version
