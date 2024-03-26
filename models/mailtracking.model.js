f72f4efe4c2b0a3cec23254d24d4192f2d5f2fd0
sravanti2300 <sravanti.kanchi@gmail.com>
2024-03-26 00:06:32 -0400

    fixes

--- models/mailtracking.model.js
+++ models/mailtracking.model.js
@@ -1,19 +1,15 @@
 module.exports = (sequelize, Sequelize) => {
     const MailTracking = sequelize.define("MailTracking", {
-      id: {
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
-        primaryKey: true,
-      },
       mail_sent: {
         type: Sequelize.DATE,
         allowNull: true,
       },
       email: {
         type: Sequelize.STRING,
-        defaultValue: false,
-        unique: true,
+        primaryKey: true,
       },
+    },{
+      timestamps: false,
     });
     return MailTracking;
   };
