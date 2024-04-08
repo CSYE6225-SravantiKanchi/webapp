105bfe7aa49fec156209849bb4fad5e91bb6889a
sravanti2300 <sravanti.kanchi@gmail.com>
2024-04-08 18:18:01 -0400

    reverting the changes

--- packerconfig/scripts/setup.sh
+++ packerconfig/scripts/setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -e
+#set -e
 
 sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 #Making the libraries Upto Date
-sudo dnf update -y || echo "Error occurred while updating packages"
+#sudo dnf update -y || echo "Error occurred while updating packages"
 
 #install unzip
 sudo dnf install -y unzip
